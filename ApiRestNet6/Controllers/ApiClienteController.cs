7bed015bdf60d7e86ac9d99d2201935af38d4869
LuisFernandoAlmeida <luisfernando78@yahoo.com>
2023-02-22 11:36:03 -0500

    Primera subida del proyecto - 22-02-2023

---
+++ ApiRestNet6/Controllers/ApiClienteController.cs
@@ -0,0 +1,33 @@
+﻿using ApiRestNet6.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ApiRestNet6.Controllers
+{
+    [Route("api/Cliente")]
+    [ApiController]
+    public class ApiClienteController : ControllerBase
+    {
+        [HttpGet ("GetHolaMundo")]
+        public string GetHolaMundo()
+        {
+            return "Hola Mundo...";
+        }
+        [HttpGet("GetEntero")]
+        public int GetEntero()
+        {
+            return 5000;
+        }
+        [HttpGet("GetClaseCliente")]
+        public ClienteEN GetClaseCliente()
+        {
+           var obj=new ClienteEN();
+           obj.id = 1;
+           obj.Nombre = "Luis";
+           return obj;
+        }
+    }
+}
---
+++ ApiRestNet6/Entity/ClienteEN.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiRestNet6.Entity
+{
+    public class ClienteEN
+    {
+        public int id { get; set; } = 0;
+        public string Nombre { get; set; } = String.Empty;
+    }
+}
+
